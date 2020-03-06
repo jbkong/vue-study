6513817609bb9338c88aa0b088c016f4162244e9
ballgaveship <ballgaveship@gmail.com>
2020-03-07 00:19:51 +0900

    caption lv 1 master

---
+++ captain/age_of_vuejs/lv1/vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-header v-on:renew="renewStr" :propsdata="str"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data : function() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'renew!'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
