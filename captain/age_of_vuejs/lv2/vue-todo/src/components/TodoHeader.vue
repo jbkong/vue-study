2cc498a87fff9398e42813f41e76c6b920421e9b
ballgaveship <ballgaveship@gmail.com>
2020-03-07 14:10:51 +0900

    initial create todo app(not reactive)

---
+++ captain/age_of_vuejs/lv2/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+      <h1>TODO it!</h1>
+  </header>
+</template>
+
+<!-- scoped 컴퍼넌트 안에서만 유효한 css 스타일이 된다.-->
+<style scodped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0.15rem;
+}
+</style>
