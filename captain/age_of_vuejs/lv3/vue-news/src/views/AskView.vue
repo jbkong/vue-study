c8086238d434aee54d9a643f1c23cd4494de6d4c
ballgaveship <ballgaveship@gmail.com>
2020-03-16 07:07:54 +0900

    lv3 int

---
+++ captain/age_of_vuejs/lv3/vue-news/src/views/AskView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p v-for="item in fetchedAsk" v-bind:key="item.title">
+      <a :href="item.url">
+        {{ item.title }}
+      </a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters([
+      'fetchedAsk'
+    ]),
+    // ...mapState({
+    //   ask: state => state.ask
+    // })
+  },
+  created() {
+    this.$store.dispatch('FETCH_ASK')
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ captain/age_of_vuejs/lv3/vue-news/src/views/JobsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p v-for="item in this.$store.state.jobs" v-bind:key="item.title">
+      <a :href="item.url">
+        {{ item.title }}
+      </a>
+      <small>{{ item.time_ago }},  {{ item.domain }}</small>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch('FETCH_JOBS')
+  },
+}
+</script>
+
+<style>
+
+</style>
